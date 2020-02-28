8ccd9995d3611700ce1da2be08c5bd18dcf0ed4c
glasdam95 <59728188+glasdam95@users.noreply.github.com>
2020-02-28 13:38:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-export WORKON_HOME=~/.virtualenvs
-source ~/.bashrc
+echo "export WORKON_HOME=~/.virtualenvs" >> .bashrc
+echo "VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'" >> .bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> .bashrc
+
+source .bashrc
 echo -e "\e[33mDone Export"
 
 git clone https://github.com/zardus/ctf-tools.git
 ./ctf-tools/bin/manage-tools setup
 echo -e "\e[33mDone git clone and setup"
 
-source ~/.bashrc
+source .bashrc
 workon ctftools
